e219755d688d15ff7cb0de6ee1644bc50fad3ab5
omar <mann002579@gmail.com>
2014-07-08 17:37:44 -0500

    :)

--- js/marcador.js
+++ js/marcador.js
@@ -5,31 +5,60 @@ $(document).ready(function() {
 
   $( "button" ).click(function() {
     var text = $( this ).text();
-    console.log(text);
-    if (text == "player1") {
-      player1=point(player1,player2);
-    }
-    else
-      {player2=point(player2,player2);}
-    
+    point($( this ).val());
   });
 
 });
-
-function point(playera,playerb){
-  switch(playera){
-    case 2:
-          switch(playerb){
-            case 2: console.log(puntos[playera]+" - all");
-            break;
-            case 3: playera++; console.log(douse);
-            break;
-            default: playera++;console.log(puntos[playera]);
-          }
+function point(player){
+  switch (player){
+    case "1":
+      switch(player1){
+              case 2:
+                    switch(player2){
+                      case 3: player1++; console.log("douse");
+                      break;
+                      default: player1++;console.log(puntos[player1]);
+                    }
+              break;
+              case 3:
+                    switch(player2){
+                      case 3: player1++; console.log("ventaja player1");
+                      break;
+                      case 4: player2=3;console.log("douse");
+                      break;
+                      default: player1=0;player2=0;console.log("player1 wins");
+                    }
+              break;      
+              case 4: player1=0;player2=0;console.log("player1 wins");
+              break;      
+              default: player1++; console.log(puntos[player1]+" - "+puntos[player2]);      
+            }
     break;
-    default: playera++;console.log(puntos[playera]);      
+    case "2":
+      switch(player2){
+              case 2:
+                    switch(player1){
+                      case 3: player2++; console.log("douse");
+                      break;
+                      default: player2++;console.log(puntos[player1]+" - "+puntos[player2]);
+                    }
+              break;
+              case 3:
+                    switch(player1){
+                      case 3: player2++; console.log("ventaja player2");
+                      break;
+                      case 4: player1=3;console.log("douse");
+                      break;
+                      default: player1=0;player2=0;console.log("player2 wins");
+                    }
+              break;      
+              case 4: player1=0;player2=0;console.log("player2 wins");
+              break;      
+              default: player2++; console.log(puntos[player1]+" - "+puntos[player2]);      
+            }
+    break;
   }
-return playera;  
+
 }
 
 function score(){
